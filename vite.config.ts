e4caa2c8c92157eb145c6d017bcd8955d0cf4905
明远湖之鱼 <488548816@qq.com>
2024-04-21 15:14:03 +0800

    feat: 配置修改

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,14 @@ import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    open: true,
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+	plugins: [react()],
+	server: {
+		open: true
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src')
+		}
+	}
 });
